fe8f1021ca483b5f2b3ebc6ff9c8f82b26c22c43
Harald Kjellson-Freij <harald.kjellson-freij@zenuity.com>
2020-01-09 13:37:25 +0100

    Solved day 13, with manual or automatic play

--- 2019/13.py
+++ 2019/13.py
@@ -4,7 +4,7 @@ import numpy
 import sys
 import termios
 import tty
-import pygame
+import time
 from PIL import Image
 
 
@@ -18,6 +18,34 @@ def getch():
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return ch
+
+
+EMPTY = 0
+WALL = 1
+BLOCK = 2
+PADDLE = 3
+BALL = 4
+
+
+def print_board(board, score):
+    print('\n\n\n')
+    for l in board:
+        for c in l:
+            if c == EMPTY:
+                print(' ', end='')
+            elif c == WALL:
+                print('=', end='')
+            elif c == BLOCK:
+                print('x', end='')
+            elif c == PADDLE:
+                print('-', end='')
+            elif c == BALL:
+                print('O', end='')
+            else:
+                print('')
+                exit
+        print('')
+    print(score)
 
 
 input_file_name = os.path.normpath(os.path.join(os.path.abspath(__file__), '../input/13_program.txt'))
@@ -36,34 +64,50 @@ while (board[-1, :] == 0).all():
     board = numpy.delete(board, board.shape[0]-1, 0)
 unique, counts = numpy.unique(board, return_counts=True)
 print(dict(zip(unique, counts))[2])
-Image.fromarray(255*board/board.max()).show()
 
 # Part 2
 program[0] = 2
 board = numpy.zeros((100, 100))
+score = 0
 output, curr_pos, stop_reason, offset_base = intcode.intcode(program, 0, 0, [])
 assert(stop_reason) == intcode.WAITING
 for i in range(0, len(output), 3):
     board[output[i], output[i+1]] = output[i+2]
+    if output[i+2] == PADDLE:
+        paddle_x = output[i]
+    elif output[i+2] == BALL:
+        ball_x = output[i]
 while (board[:, -1] == 0).all():
     board = numpy.delete(board, board.shape[1]-1, 1)
 while (board[-1, :] == 0).all():
     board = numpy.delete(board, board.shape[0]-1, 0)
+numpy.set_printoptions(threshold=sys.maxsize)
+print("Press m for manual playing")
+char = getch()
+manual = True if char == 'm' else False
 
 while stop_reason == intcode.WAITING:
-    char = getch()
-    if char == "a":
-        direction = -1
-    elif char == "d":
-        direction = 1
+    if manual:
+        print_board(board, score)
+        char = getch()
+        if char == "a":
+            direction = -1
+        elif char == "d":
+            direction = 1
+        else:
+            direction = 0
     else:
-        direction = 0
+        direction = numpy.sign(ball_x-paddle_x)
+
     output, curr_pos, stop_reason, offset_base = intcode.intcode(
         program, curr_pos=curr_pos, offset_base=offset_base, input_values=[direction])
-    image = Image.fromarray(255*board/board.max())
-    image.resize(imageshow()
-
-
-unique, counts=numpy.unique(board, return_counts=True)
-print(dict(zip(unique, counts))[2])
-Image.fromarray(255*board/board.max()).resshow()
+    for i in range(0, len(output), 3):
+        if(output[i] == -1 and output[i+1] == 0):
+            score = output[i+2]
+        else:
+            board[output[i], output[i+1]] = output[i+2]
+            if output[i+2] == PADDLE:
+                paddle_x = output[i]
+            elif output[i+2] == BALL:
+                ball_x = output[i]
+print(score)
