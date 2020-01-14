5e4f7121b312897933812a3c6268e4e7c7468882
Harald Kjellson-Freij <harald.kjellson-freij@zenuity.com>
2020-01-14 14:19:12 +0100

    Day 15
    Probably an inefficient method, and ugly as hell (very much copied
    code between oxygen search and depth search), but it does the job

--- 2019/15.py
+++ 2019/15.py
@@ -38,7 +38,25 @@ def step(dir):
         return numpy.array([0, 1])
 
 
-def investigate(distances, pos, best_distance, program, curr_pgm_pos, offset_base):
+def print_board(distances):
+    numpy.set_printoptions(threshold=sys.maxsize)
+    numpy.set_printoptions(linewidth=200)
+    x_min = 0
+    x_max = 0
+    y_min = 0
+    y_max = 0
+    for pos in distances:
+        x_min = min(x_min, pos[0])
+        x_max = max(x_max, pos[0])
+        y_min = min(y_min, pos[1])
+        y_max = max(y_max, pos[1])
+    board = numpy.zeros((x_max-x_min+1, y_max-y_min+1))
+    for pos in distances:
+        board[(pos[0]-x_min, pos[1]-y_min)] = distances[pos]
+    print(board)
+
+
+def find_oxygen(distances, pos, best_distance, program, curr_pgm_pos, offset_base):
     d = distances[pos]
     for direction in [NORTH, SOUTH, EAST, WEST]:
         output, curr_pgm_pos, stop_reason, offset_base = intcode.intcode(
@@ -48,11 +66,13 @@ def investigate(distances, pos, best_distance, program, curr_pgm_pos, offset_bas
             new_pos = tuple(numpy.array(pos)+step(direction))
             if new_pos not in distances or d+1 < distances[new_pos]:
                 distances[new_pos] = d+1
-                if output == OXYGEN:
-                    print("OXYGEN!!!!")
+                if output[0] == OXYGEN:
                     best_distance = distances[new_pos]
+                    oxygen_distances = {new_pos: 0}
+                    find_deepest(oxygen_distances, new_pos, program, curr_pgm_pos, offset_base)
+                    print("Time for oxygen filling: {}".format(max(oxygen_distances.values())))
                 else:
-                    best_distance = min(best_distance, investigate(
+                    best_distance = min(best_distance, find_oxygen(
                         distances, new_pos, best_distance, program, curr_pgm_pos, offset_base))
             output, curr_pgm_pos, stop_reason, offset_base = intcode.intcode(
                 program, curr_pos=curr_pgm_pos, offset_base=offset_base, input_values=[back(direction)])
@@ -61,7 +81,21 @@ def investigate(distances, pos, best_distance, program, curr_pgm_pos, offset_bas
     return best_distance
 
 
-distances = {}
-pos = (0, 0)
-distances[pos] = 0
-print("Distance to oxygen: {}".format(investigate(distances, pos, 10000000, program, 0, 0)))
+def find_deepest(distances, pos, program, curr_pgm_pos, offset_base):
+    d = distances[pos]
+    for direction in [NORTH, SOUTH, EAST, WEST]:
+        output, curr_pgm_pos, stop_reason, offset_base = intcode.intcode(
+            program, curr_pos=curr_pgm_pos, offset_base=offset_base, input_values=[direction])
+        assert len(output) == 1
+        if not output[0] == WALL:
+            new_pos = tuple(numpy.array(pos)+step(direction))
+            if new_pos not in distances or d+1 < distances[new_pos]:
+                distances[new_pos] = d+1
+                find_deepest(distances, new_pos, program, curr_pgm_pos, offset_base)
+            output, curr_pgm_pos, stop_reason, offset_base = intcode.intcode(
+                program, curr_pos=curr_pgm_pos, offset_base=offset_base, input_values=[back(direction)])
+            assert len(output) == 1
+            assert not output[0] == WALL
+
+
+print("Distance to oxygen: {}".format(find_oxygen({(0, 0): 0}, (0, 0), 10000000, program.copy(), 0, 0)))
